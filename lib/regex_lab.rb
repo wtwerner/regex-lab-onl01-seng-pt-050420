4400f15a90800d0f2e23f2d143b1caf80e85c3df
Tommy Werner <wtwerner@gmail.com>
2020-05-22 02:05:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,8 +14,8 @@ def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
+def first_word_capitalized_and_ends_with_punctuation?(text) 
+  text.scan(/)
 end
 
 def valid_phone_number?(phone)
