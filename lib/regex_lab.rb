23fb3014da66986c984b880b8219c6f21ba328d8
Tommy Werner <wtwerner@gmail.com>
2020-05-22 02:10:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text) 
-  text.scan(/\b\w\W\b/)
+   five_words = []
+  five_words << text.scan(/\b\w\W\b/)
+  five_words.is_empty?
 end
 
 def valid_phone_number?(phone)
