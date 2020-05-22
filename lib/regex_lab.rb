603bf93457908c2e978b280c6319ed56faeb0b6e
Tommy Werner <wtwerner@gmail.com>
2020-05-22 02:15:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text) 
-   text.first.match(/[A-Z]/.../\W\/)
+   text.first.match(/\[A-Z]\\W\/)
 end
 
 def valid_phone_number?(phone)
