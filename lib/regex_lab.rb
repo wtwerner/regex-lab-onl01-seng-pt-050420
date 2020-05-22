17153f2a398f8fc2be37c58d7d52785f42c71243
Tommy Werner <wtwerner@gmail.com>
2020-05-22 02:27:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4})
+  if phone.match((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4})
     true
   else
     false
