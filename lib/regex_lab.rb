c9e7b519947a95ee05283e78ca60b6de8a44e27c
Tommy Werner <wtwerner@gmail.com>
2020-05-22 02:19:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/(\d)
+  if phone.match(/(\d+)-(\d+)-(\d+)/)
+    true
+  else
+    false
+  end
 end
